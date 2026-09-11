9b476c7656e69f683394b2cc817938b0e5ee7ed0
Heinz Schumacher <heinz@unai.edu>
2026-09-11 10:03:40 +0700

    Masukkan nama anda + Masukkan NIM anda

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nama anda: ");
+        String Masukkan_nama_anda = scanner.nextLine();
+
+        System.out.print("Masukkan NIM anda: ");
+        int Masukkan_NIM_anda = scanner.nextInt();
+
+        System.out.print("Masukkan umur anda: ");
+        int Masukkan_umur_anda = scanner.nextInt();
+
+        System.out.print("Masukkan tinggi anda: ");
+        double Masukkan_tinggi_anda = scanner.nextDouble();
+
+        scanner.nextLine();
+
+        System.out.print("Masukkan asal kota anda: ");
+        String Masukkan_asal_kota_anda = scanner.nextLine();
+
+
+    }
+    }
